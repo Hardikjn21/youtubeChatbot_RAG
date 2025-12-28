3cbe3601cda853af1e3ea13e705437e86ac27889
Hardik Jain <hardik.jain@iitg.ac.in>
2025-12-28 14:33:46 +0530

    ollama phi4-min working well

--- src/project/components/llm.py
+++ src/project/components/llm.py
@@ -2,3 +2,7 @@ from langchain_community.llms import Ollama
 
 def get_llm():
     return Ollama(model="phi4-mini", temperature=0.2)
+
+if __name__ == "__main__":
+    llm=get_llm()
+    print(llm.invoke("who is Virat Kohli"))
