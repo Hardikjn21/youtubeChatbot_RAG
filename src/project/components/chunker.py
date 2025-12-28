f000492ddf501e9345fac50afe6efd129c86366f
Hardik Jain <hardik.jain@iitg.ac.in>
2025-12-28 14:46:41 +0530

    chunker works

--- src/project/components/chunker.py
+++ src/project/components/chunker.py
@@ -1,8 +1,14 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 def chunk_text(text: str):
-    splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    return splitter.split_text(text)
+  text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=0)
+  return text_splitter.split_text(text)
 
 def join_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
+
+
+if __name__ == "__main__":
+   chunks = chunk_text("hello world " * 100)
+   print(chunks)
+   print(f"Number of chunks: {len(chunks)}")
